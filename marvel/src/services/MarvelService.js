23ac320142e6e2f9582460e7bd11766d2b17afb6
dibrugh <dibrugh@gmail.com>
2023-11-04 02:30:38 +0300

    feat: write functions to get only necessary data from the server

--- marvel/src/services/MarvelService.js
+++ marvel/src/services/MarvelService.js
@@ -6,7 +6,7 @@ class MarvelService {
     _apiKey = 'apikey=f82fb1b4de23f8833770901066aedc4c';
     // Создаём функцию, которая будет возвращать данные в формате JSON
     getResource = async (url) => {
-        let res = await fetch (url);
+        let res = await fetch(url);
 
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
@@ -15,13 +15,27 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        // Преобразовываем каждый элемент
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?
-        limit=9&offset=210&${this._apiKey}`);
+    // Т.к getResource асинхронная
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    // Функция, которая формирует объект только с нужными данными для избежания дублирования кода в других компонентах
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description.length ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
     }
 }
 
